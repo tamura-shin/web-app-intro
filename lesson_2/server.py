2adb5b2c0a0b59fec85873e1b77aa4c5d5773907
kappaexpress <46478065+kappaexpress@users.noreply.github.com>
2025-04-26 09:37:45 +0000

    add

---
+++ lesson_2/server.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import os
+import uvicorn
+
+app = FastAPI()
+
+BASE_DIR = os.path.dirname(__file__)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_html():
+    html_file_path = os.path.join(BASE_DIR, "client.html")
+    with open(html_file_path, "r", encoding="utf-8") as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="127.0.0.1", port=8000, reload=True)
