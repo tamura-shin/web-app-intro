6203262c5d437363d116c4a80eb9b6fd7e94007f
kappaexpress <46478065+kappaexpress@users.noreply.github.com>
2025-04-28 12:52:57 +0000

    README.mdのlesson_8フォルダへの移動手順を修正し、データ詳細取得の手順を明確化。server.pyのSQLクエリを修正。

--- lesson_8/server.py
+++ lesson_8/server.py
@@ -60,7 +60,7 @@ def read_data_item(item_id: int):
 
     # TODO: 課題　idに対応するデータを取得するSQLをexecute内に書く
     # テーブル名は data
-    item = conn.execute("SELECT * FROM data WHERE id = ?", item_id).fetchone()
+    item = conn.execute("", item_id).fetchone()
 
     # データベース接続を閉じる (リソースの解放)
     conn.close()
